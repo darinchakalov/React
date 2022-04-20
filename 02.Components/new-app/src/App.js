590c434910c63236ba0959a80aa00f1c54231536
darinchakalov <darinchakalov@hotmail.com>
2022-04-20 08:56:09 +0300

    init new app for components lecture

---
+++ 02.Components/new-app/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>ToDo App</h1>
+			</header>
+
+			<main></main>
+
+			<footer>
+				<p>All rights reserved &copy;</p>
+			</footer>
+		</div>
+	);
+}
+
+export default App;
