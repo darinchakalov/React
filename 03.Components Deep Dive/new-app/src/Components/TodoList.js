0027f4d6a9773918b0570658688d63b0c480cffe
darinchakalov <darinchakalov@hotmail.com>
2022-04-21 18:39:53 +0300

    components Lifecycle

--- 03.Components Deep Dive/new-app/src/Components/TodoList.js
+++ 03.Components Deep Dive/new-app/src/Components/TodoList.js
@@ -1,15 +1,40 @@
+import { useState } from "react";
+import uniqid from "uniqid";
+
+import TodoItem from "./TodoItem.js";
+
 export default function TodoList() {
-	const todos = [
+	const [todos, setTodos] = useState([
 		{ id: 1, text: "Clean my room" },
 		{ id: 2, text: "Wash the dishes" },
 		{ id: 3, text: "Go to the gym" },
-	];
+	]);
+
+	const onChangeHandler = (e) => {
+		let todo = { id: uniqid(), text: e.target.value };
+
+		setTodos((oldTodos) => [...oldTodos, todo]);
+
+		e.target.value = "";
+	};
+
+	const deleteTodoItemClickHandler = (id) => {
+		setTodos((oldTodos) => oldTodos.filter((x) => x.id !== id));
+	};
 
 	return (
-		<ul>
-			{todos.map((l) => (
-				<li key={l.id}>{l.text}</li>
-			))}
-		</ul>
+		<>
+			<input type="text" name="todo" onBlur={onChangeHandler} />
+			<ul>
+				{todos.map((todo) => (
+					<TodoItem
+						key={todo.id}
+						id={todo.id}
+						text={todo.text}
+						onDelete={deleteTodoItemClickHandler}
+					></TodoItem>
+				))}
+			</ul>
+		</>
 	);
 }
