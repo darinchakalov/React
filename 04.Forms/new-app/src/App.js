8f464c6c1a9a1a90bf284da6b5c5e804a6ec035f
darinchakalov <darinchakalov@hotmail.com>
2022-05-04 18:32:13 +0300

    example of controlled form

--- 04.Forms/new-app/src/App.js
+++ 04.Forms/new-app/src/App.js
@@ -1,23 +1,44 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+	let [username, setUsername] = useState("Pesho");
+
+	const onChangeHandler = (e) => {
+		setUsername(e.currentTarget.value);
+	};
+
 	const submitHandler = (e) => {
 		e.preventDefault();
 
 		let formData = new FormData(e.currentTarget);
 		let { username, password } = Object.fromEntries(formData);
-		
-
-    
+		console.log(username);
 	};
 
 	return (
 		<div className="App">
 			<form method="POST" onSubmit={submitHandler}>
-				<label htmlFor="username">Name</label>
-				<input type="text" name="username" />
-				<label htmlFor="password">Password</label>
-				<input type="password" name="password" />
+				<div>
+					<label htmlFor="username">Name</label>
+					<input type="text" name="username" value={username} onChange={onChangeHandler} />
+				</div>
+				<div>
+					<label htmlFor="password">Password</label>
+					<input type="password" name="password" defaultValue="asd" />
+				</div>
+				<div>
+					<label htmlFor="services">Services</label>
+					<select name="services">
+						<option value="1">Private client</option>
+						<option value="2">Business client</option>
+						<option value="3">Enterprise client</option>
+					</select>
+				</div>
+				<div>
+					<label htmlFor="rememberme">Remember me</label>
+					<input type="checkbox" name="rememberme" defaultChecked />
+				</div>
 				<button type="submit">Login</button>
 			</form>
 		</div>
