6c647313573d74c75964b30ea035fe775b097914
darinchakalov <darinchakalov@hotmail.com>
2022-05-05 17:25:26 +0300

    example of useState

--- 05.React Hooks/new-app/src/App.js
+++ 05.React Hooks/new-app/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	const stateResult = useState("");
+
+	const name = stateResult[0];
+	const setName = stateResult[1];
+
+	useEffect(() => {
+		setTimeout(() => {
+			setName("Pesho");
+		}, 1500);
+	}, []);
+
+	return (
+		<div className="App">
+			<h2>{!name ? "Loading..." : name}</h2>
+		</div>
+	);
 }
 
 export default App;
